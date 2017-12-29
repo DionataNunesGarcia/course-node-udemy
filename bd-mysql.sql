ebd59e6a2b5feaebb05ca99d62f56b5f6416a99f
dionataCursos <dyonathankinder@gmail.com>
2017-12-29 17:10:52 -0200

    add bd

---
+++ bd-mysql.sql
@@ -0,0 +1,11 @@
+INSERT INTO node_udemy.noticias (id,titulo,noticia,data_criacao,data_noticia,autor,resumo) VALUES 
+(1,'titulo noticia','conteudo noticia','2017-12-28 10:12:29.000',NULL,NULL,NULL)
+,(2,'titulo noticia 2','conteudo noticia 2','2017-12-28 10:14:01.000',NULL,NULL,NULL)
+,(6,'asdasdasd','sdfsdfsdf','2017-12-28 15:08:56.000',NULL,NULL,NULL)
+,(4,'titulo noticia 3','conteudo noticia 3','2017-12-28 10:14:21.000',NULL,NULL,NULL)
+,(5,'titulo noticia 4','conteudo noticia 4','2017-12-28 10:14:26.000',NULL,NULL,NULL)
+,(7,'aaaaaaaaaaa','bvbbbbbbbbbbbbb','2017-12-28 15:09:14.000',NULL,NULL,NULL)
+,(8,'aaaaaaaaaaabbbbbbbbbbbbbbbbbbb','bvbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaa','2017-12-28 15:15:40.000',NULL,NULL,NULL)
+,(9,'Sistema OS','asdasdasdasdasdasdasdasd','2017-12-28 16:29:50.000','2017-12-28','asdasd','dasdasdasdasdasdasd')
+,(10,'asdasdasdsdfsdfs','sdfsdfsdfsdf','2017-12-28 16:52:32.000','2017-12-13','sdfsdfsdf','sdfsdfsdfsdfsdfsdfsdfsdf')
+;
